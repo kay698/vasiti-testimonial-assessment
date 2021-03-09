9ebcec3d933155af61ae2ab64095cfb3f32d9f6d
kel <ogbukakelechi20@gmail.com>
2021-03-09 11:49:05 +0100

    footer finalized with assets added

--- src/components/Footer/footer.styles.js
+++ src/components/Footer/footer.styles.js
@@ -4,6 +4,89 @@ export const useStyles = makeStyles((theme) => ({
   footerGrid: {
     backgroundColor: "#25201D",
     height: "auto",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  footerGrid__HeaderSection: {
+    backgroundColor: "inherit",
+    padding: "1rem 2rem",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    color: "#F3F3F3 !important",
+
+    "@media (max-width: 920px)": {
+      flexDirection: "column-reverse",
+      justifyContent: "center",
+      alignItems: "center",
+      borderBottom: "1px solid #bbb",
+    },
+
+    [theme.breakpoints.down("md")]: {
+      padding: 0,
+    },
+  },
+
+  imageWrap: {
+    zIndex: 100000,
+    marginTop: "-80px",
+    "@media (max-width: 920px)": {
+      margin: "40px 0",
+      marginRight: "-25px",
+      "& img": {
+        width: "400px",
+        height: "380px",
+      },
+    },
+    "@media (max-width: 420px)": {
+      "& img": {
+        width: "320px",
+        height: "300px",
+        marginRight: "0",
+      },
+    },
+  },
+  HeaderSection__textsWrap: {
+    "& h4": {
+      fontWeight: 700,
+      fontSize: "40px",
+      margin: "20px 0",
+      "@media (max-width: 500px)": {
+        fontSize: "30px"
+      },
+    },
+    "@media (max-width: 400px)": {
+      marginLeft: "10px",
+    },
+
+  },
+  HeaderSection__subText:{
+   width: "350px"
+  },
+  HeaderSection__inputWrap: {
+    width: "400px",
+    background: "rgba(255, 255, 255, 0.2)",
+    borderRadius: "6px",
+    height: "50px",
+    marginTop: "20px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "0 10px",
+    "@media (max-width: 500px)": {
+      width: "310px"
+    },
+
+    "& input": {
+      height: "100%",
+      paddingLeft: "10px",
+      "&::placeholder": {
+        color: "white",
+      },
+    },
+  },
+
+  footerGrid__linkSection: {
+    backgroundColor: "inherit",
     padding: "1rem 2rem",
     flexDirection: "column",
     justifyContent: "center",
@@ -20,25 +103,23 @@ export const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
     alignItems: "start",
 
-
     // footer link items with media query
     "& a": {
       color: "#F3F3F3",
       textDecoration: "none",
-      fontSize:"14px",
-      lineHeight:"16.41px",
-      fontWeight:"400",
+      fontSize: "14px",
+      lineHeight: "16.41px",
+      fontWeight: "400",
 
       "&:hover": {
         color: "#bbb",
       },
 
-      "@media (max-width: 510px)" : {
+      "@media (max-width: 510px)": {
         fontSize: "12px",
         margin: "0",
       },
     },
-
 
     //media query for links body
     "@media (max-width: 1110px)": {
@@ -52,7 +133,7 @@ export const useStyles = makeStyles((theme) => ({
       flexWrap: "wrap",
 
       "& .MuiGrid-item": {
-        width: "50%",
+        width: "30%",
         marginBottom: "3rem",
       },
     },
@@ -71,12 +152,10 @@ export const useStyles = makeStyles((theme) => ({
 
       "& .MuiGrid-item": {
         width: "max-content",
-        minWidth:"40%",
+        minWidth: "40%",
         marginBottom: "3rem",
       },
     },
-
-    
 
     //  Headers (H6)
     "& h6": {
@@ -95,7 +174,6 @@ export const useStyles = makeStyles((theme) => ({
       },
     },
   },
-
 
   // socia media icons section
   socials: {
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,51 +10,78 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import { useStyles } from "./footer.styles";
+import PhoneImage from "../../assets/footer_image.png";
+import { CustomButton } from "../Buttons";
 
 const Footer = () => {
   const classes = useStyles();
 
   return (
     <>
-      <Grid container className={classes.footerGrid}>
-        <Grid className={classes.linksWrap}>
-          {FooterNavItems.map((list, index) => (
-            <Grid item key={index}>
-              <Typography variant={"h6"}>{list.header}</Typography>
-              <MenuList>
-                {list.items.map((item, idx) => (
-                  <MenuItem key={idx}>
-                    <a>{item}</a>
-                  </MenuItem>
-                ))}
-              </MenuList>
+      <Grid container container className={classes.footerGrid}>
+        <Grid container className={classes.footerGrid__HeaderSection}>
+          <Grid className={classes.imageWrap}>
+            <div className={classes.imageWrap__container}>
+              <img src={PhoneImage} alt="phone image" />
+            </div>
+          </Grid>
+          <Grid className={classes.HeaderSection__textsWrap}>
+            <Typography variant={"h4"} className={classes.HeaderSection__headerText}>
+              Be a member <br />
+              of our community 🎉
+            </Typography>
+            <Typography className={classes.HeaderSection__subText}>
+              We’d make sure you’re always first to know what’s
+              happening on Vasiti—thus, the world.
+            </Typography>
+            <div  className={classes.HeaderSection__inputWrap} >
+              <input type="email" placeholder="enter your email address"/>
+              <CustomButton text="subscribe" click={() => {}} sm  bngColor="white" color="#242120" />
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container className={classes.footerGrid__linkSection}>
+          <Grid className={classes.linksWrap}>
+            {FooterNavItems.map((list, index) => (
+              <Grid item key={index}>
+                <Typography variant={"h6"}>{list.header}</Typography>
+                <MenuList>
+                  {list.items.map((item, idx) => (
+                    <MenuItem key={idx}>
+                      <a>{item}</a>
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </Grid>
+            ))}
+            <Grid item>
+              <Typography variant={"h6"} clasclassName={classes.socialsHeader}>
+                Join Our Community
+              </Typography>
+              <section className={classes.socials}>
+                <a href="/" target={"_blank"}>
+                  <IconButton>
+                    <FacebookIcon />
+                  </IconButton>
+                </a>
+                <a href="/" target={"_blank"}>
+                  <IconButton>
+                    <InstagramIcon />
+                  </IconButton>
+                </a>
+                <a href="/" target={"_blank"}>
+                  <IconButton>
+                    <TwitterIcon />
+                  </IconButton>
+                </a>
+                <a href="/" target={"_blank"}>
+                  <IconButton>
+                    <LinkedInIcon />
+                  </IconButton>
+                </a>
+              </section>
+              <Typography>Email Newsletter</Typography>
             </Grid>
-          ))}
-          <Grid item>
-            <Typography variant={"h6"} clasclassName={classes.socialsHeader}>Join Our Community</Typography>
-            <section className={classes.socials}>
-              <a href="/" target={"_blank"}>
-                <IconButton>
-                  <FacebookIcon />
-                </IconButton>
-              </a>
-              <a href="/" target={"_blank"}>
-                <IconButton>
-                  <InstagramIcon />
-                </IconButton>
-              </a>
-              <a href="/" target={"_blank"}>
-                <IconButton>
-                  <TwitterIcon />
-                </IconButton>
-              </a>
-              <a href="/" target={"_blank"}>
-                <IconButton>
-                  <LinkedInIcon />
-                </IconButton>
-              </a>
-            </section>
-            <Typography>Email Newsletter</Typography>
           </Grid>
         </Grid>
       </Grid>
